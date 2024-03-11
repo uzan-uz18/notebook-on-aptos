bd02e0795ddeb2d4056b6c519d45c1b092d45a36
uzan <dadahu0018@gamil.com>
2024-03-11 12:44:12 +0800

    add list function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Layout, Row, Col, Button, Spin } from "antd";
+import {Layout, Row, Col, Button, Spin, List, Checkbox} from "antd";
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import { Aptos } from "@aptos-labs/ts-sdk";
@@ -13,20 +13,28 @@ import {
 
 const aptos = new Aptos();
 
+type Task={
+    address: string;
+    completed: boolean;
+    content: string;
+    task_id: string;
+}
 
 function App() {
+    const [accountHasList, setAccountHasList] = useState<boolean>(false);
+    // a local state to keep tract whether a transaction is in progress
+    const [transactionInProgress, setTransactionInProgress] =
+        useState<boolean>(false);
     // Extract the account object from the wallet adapter
     const { account,signAndSubmitTransaction }=useWallet();
+    const [tasks, setTasks]=useState<Task[]>([]);
 
     // add a hook to update the account resource changes
     useEffect(() => {
         fetchList();
     }, [account?.address]);
 
-    const [accountHasList, setAccountHasList] = useState<boolean>(false);
-    // a local state to keep tract whether a transaction is in progress
-    const [transactionInProgress, setTransactionInProgress] =
-        useState<boolean>(false);
+
 
     const moduleAddress = "0xa85172e8c0f2125ca6b11b2774b45f3c39540c2b799583eb6771ae29c7ef2939";
 
@@ -40,6 +48,25 @@ function App() {
                 }
             );
             setAccountHasList(true);
+
+            // tasks table handle
+            const tableHandle = (todoListResource as any).tasks.handle;
+            // tasks table counter
+            const taskCounter = (todoListResource as any).task_counter;
+
+            let tasks=[];
+            let counter=1;
+            while(counter <= taskCounter) {
+                const tableItem = {
+                    key_type: "u64",
+                    value_type: `${moduleAddress}::todolist::Task`,
+                    key: `${counter}`,
+                };
+                const task = await aptos.getTableItem<Task>({handle:tableHandle, data:tableItem});
+                tasks.push(task);
+                counter++;
+            }
+            setTasks(tasks);
         } catch (e: any){
             setAccountHasList(false);
         }
@@ -81,20 +108,48 @@ function App() {
                 </Row>
             </Layout>
             <Spin spinning = {transactionInProgress}>
-            {!accountHasList && (
-                <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
-                    <Col span={8} offset={8}>
-                        <Button
-                            onClick={addNewList}
-                            block
-                            type="primary"
-                            style={{ height: "40px", backgroundColor: "#3f67ff" }}
-                        >
-                            Add new list
-                        </Button>
-                    </Col>
-                </Row>
-            )}
+                {
+                    !accountHasList ? (
+                        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+                            <Col span={8} offset={8}>
+                                <Button
+                                    disabled={!account}
+                                    block
+                                    onClick={addNewList}
+                                    type="primary"
+                                    style={{ height: "40px", backgroundColor: "#3f67ff" }}
+                                >
+                                    Add new list
+                                </Button>
+                            </Col>
+                        </Row>
+                    ) : (
+                        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+                            <Col span={8} offset={8}>
+                                {tasks && (
+                                    <List
+                                        size="small"
+                                        bordered
+                                        dataSource={tasks}
+                                        renderItem={(task: any) => (
+                                            <List.Item actions={[<Checkbox />]}>
+                                                <List.Item.Meta
+                                                    title={task.content}
+                                                    description={
+                                                        <a
+                                                            href={`https://explorer.aptoslabs.com/account/${task.address}/`}
+                                                            target="_blank"
+                                                        >{`${task.address.slice(0, 6)}...${task.address.slice(-5)}`}</a>
+                                                    }
+                                                />
+                                            </List.Item>
+                                        )}
+                                    />
+                                )}
+                            </Col>
+                        </Row>
+                    )
+                }
             </Spin>
         </>
     );
